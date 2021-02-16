bd73236ffe78a7e56caaff66b0a850b43de9431b
Ivan <kjs200095@gmail.com>
2021-02-16 19:43:39 +0300

    for PC version

---
+++ lesson_1/js/main.js
@@ -0,0 +1,25 @@
+const data = [
+    {title: 'Notebook', id:1, price:2000},
+    {title: 'Keyboard', id:2, price:200},
+    {title: 'Mouse', id:3, price:100},
+    {title: 'Gamepad', id:4, price:87}
+
+];
+
+
+const renderProduct = (title, id, price) => {
+    return `
+        <div class="product-item">
+            <h3>${title}</h3>
+            <p>${price}</p>
+        </div>
+    `;
+};
+
+
+const render = (products) => {
+    const productsList = products.map(item => renderProduct(item.title, item.id, item.price));
+
+    document.querySelector(".products").innerHTML = productsList;
+};
+render(data);
